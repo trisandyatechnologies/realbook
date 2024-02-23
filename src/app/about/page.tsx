e70c25347e07da7b0e1236540f40c757e4b4cbfb
VENKATA TEJA DADI <venkatatejadadi@gmail.com>
2024-02-23 15:13:38 +0530

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,6 @@ const About = () => {
             <Typography.Title level={2} style={{ display: 'flex', justifyContent: 'center', color: 'blue' }}>
                 RealBook
             </Typography.Title>
-
             <Typography.Title level={2} style={{ display: 'flex', justifyContent: 'center', color: 'blue' }}>
                 Who We Are
             </Typography.Title>
@@ -17,11 +16,9 @@ const About = () => {
                     <Typography.Title level={2} style={{ color: 'blue' }}>
                         Global Presence
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
                     </Typography.Title>
@@ -31,31 +28,23 @@ const About = () => {
                     <Typography.Title level={2} style={{ color: 'blue' }}>
                         Responsibility
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
                     </Typography.Title>
                 </Col>
-
             </Row>
-
             <Image style={{ width: 1500 }} src='https://png.pngtree.com/thumb_back/fh260/background/20210906/pngtree-high-tech-real-estate-blue-banner-technology-image_805815.jpg' />
-
             <Row style={{ display: 'flex', justifyContent: 'space-around' }}>
-
                 <Col span={10}>
                     <Typography.Title level={2} style={{ color: 'blue' }}>
                         Our Vision
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
                     </Typography.Title>
@@ -65,16 +54,13 @@ const About = () => {
                     <Typography.Title level={2} style={{ color: 'blue' }}>
                         Our Mission
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
                     </Typography.Title>
-
                     <Typography.Title level={4}>
                         At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
                     </Typography.Title>
                 </Col>
-
             </Row>
         </Layout >
     );
