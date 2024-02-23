0444c65a9f95f9ce32449574870126ad101383f8
Naga Durga <gollanagadurga16@gmail.com>
2024-02-23 12:14:01 +0530

    prospect backend api's

---
+++ src/app/api/prospects/route.ts
@@ -0,0 +1,15 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: Response) {
+    const prospects = await prisma.prospect.findMany({});
+    return NextResponse.json(prospects);
+  }
+
+  export async function POST(req: Request, res: Response) {
+    const prospectBody = await req.json();
+    const prospect = await prisma.prospect.create({
+      data: prospectBody,
+    });
+    return NextResponse.json(prospect);
+  }
