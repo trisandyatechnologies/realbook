f5b5a84bd050f83b9aa1748f9b01299dcb2693b2
VENKATA TEJA DADI <venkatatejadadi@gmail.com>
2024-02-27 16:39:49 +0530

    modified venture crud

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,5 @@
 import { API_ROOT } from "@/utils/config";
-import { Venture } from "@prisma/client/edge";
-import { Prospect } from "@prisma/client/edge";
+import { Venture } from "@prisma/client";
 
 export const createVenture = async (VentureBody: Partial<Venture>) => {
     const venture = await fetch(`${API_ROOT}ventures`, {
@@ -13,7 +12,7 @@ export const createVenture = async (VentureBody: Partial<Venture>) => {
     return venture;
 };
 
-export const getVenture = async () => {
+export const getVentures = async () => {
     const ventures = await fetch(`${API_ROOT}ventures`).then((res) => res.json());
     return ventures;
 };
@@ -45,53 +44,6 @@ export const deleteVenture = async (ventureId: string) => {
     } 
     catch (error) {
         console.error("Error creating venture:");
-        throw error;
-    }
-};
-
-export const createProspect = async (ProspectBody: Partial<Prospect>) => {
-    const prospect = await fetch(`${API_ROOT}prospects`, {
-        method: "POST",
-        body: JSON.stringify(ProspectBody),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((res) => res.json());
-    return prospect;
-};
-
-export const getProspect = async () => {
-    const prospect = await fetch(`${API_ROOT}prospect`).then((res) => res.json());
-    return prospect;
-};
-
-export const updateprospect = async (ProspectId: string, ProspectBody: Partial<Prospect>) => {
-    const prospect = await fetch(`${API_ROOT}prospects/${ProspectId}`, {
-        method: "PUT",
-        body: JSON.stringify(ProspectBody),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((res) => res.json());
-    return prospect;
-};
-
-export const deleteProspect = async (prospectId: string) => {
-    try {
-        const response = await fetch(`${API_ROOT}prospects/${prospectId}`, {
-            method: "DELETE",  
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        if (!response.ok) {
-            throw new Error(`Failed to delete prospect: ${response.status} - ${response.statusText}`);
-        }
-        const prospect = await response.json();
-        return prospect;
-    } 
-    catch (error) {
-        console.error("Error creating Prospect:");
         throw error;
     }
 };
