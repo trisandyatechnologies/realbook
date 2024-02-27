16f75f6039e10da73600185623a8e5fb1b54c654
VENKATA TEJA DADI <venkatatejadadi@gmail.com>
2024-02-27 16:24:24 +0530

    modified about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,69 +1,84 @@
 'use client'
-import { Col, Flex, Image, Layout, Row, Typography } from 'antd';
+import { Col, Layout, Row, Typography, theme } from 'antd';
 import React from 'react';
 
+
 const About = () => {
+    const {
+        token: { padding },
+    } = theme.useToken()
+
     return (
         <Layout >
-            <Typography.Title level={2} style={{ display: 'flex', justifyContent: 'center', color: 'blue' }}>
-                RealBook
+            <Typography.Title level={2} style={{ display: 'flex', justifyContent: 'center' }}>
+                <b> Welcome to REALBOOK
+                </b>
             </Typography.Title>
-            <Typography.Title level={2} style={{ display: 'flex', justifyContent: 'center', color: 'blue' }}>
-                Who We Are
-            </Typography.Title>
-            <Row style={{ display: 'flex', justifyContent: 'space-around' }}>
-                <Col span={10} >
-                    <Typography.Title level={2} style={{ color: 'blue' }}>
-                        Global Presence
-                    </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
-                    </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
-                    </Typography.Title>
+            <Row style={{ padding: padding }}>
+                <Col span={11}>
+                    <img style={{ width: 500 }} src='https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
                 </Col>
                 <Col span={2}></Col>
-                <Col span={10} >
-                    <Typography.Title level={2} style={{ color: 'blue' }}>
-                        Responsibility
+                <Col span={11} style={{ padding: padding }}>
+                    <Typography.Title level={3} >
+                        Meet Our Team
                     </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
+                    <Typography>
+                        Meet the dedicated professionals behind REALBOOK. Our team is comprised of experienced real estate agents with a deep understanding of the market. We work collaboratively to provide you with unparalleled service and expertise.
+                    </Typography>
+                    <Typography.Title level={3} >
+                        Client Testimonials
                     </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
-                    </Typography.Title>
+                    <Typography>
+                        Don't just take our word for it. Hear what our clients have to say about their experiences with REALBOOK. Read our testimonials to discover the success stories of happy homeowners and investors.
+                    </Typography>
                 </Col>
             </Row>
-            <Flex  >
-                <Image src='https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
-            </Flex>
-            {/* <Image style={{ width: "100%", height: "20" }} src='https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' /> */}
-            <Row style={{ display: 'flex', justifyContent: 'space-around' }}>
-                <Col span={10}>
-                    <Typography.Title level={2} style={{ color: 'blue' }}>
-                        Our Vision
+            <Row style={{ padding: padding }}>
+                <Col span={11} style={{ padding: padding }}>
+                    <Typography.Title level={2} >
+                        Our Story
                     </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
+                    <Typography>
+                        Established in 2024, REALBOOk has been a trusted name in the real estate industry. Our journey began with a passion for helping individuals and families find their ideal homes. Over the years, we've grown into a dedicated team of experienced real estate agents, brokers, and support staff.
+                    </Typography>
+                    <Typography.Title level={2} >
+                        Our Mission and Values
                     </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
-                    </Typography.Title>
+                    <Typography>
+                        Our mission is to mission statement, and we achieve this by upholding our core values of values. These principles guide every interaction, transaction, and decision we make.
+                    </Typography>
                 </Col>
                 <Col span={2}></Col>
-                <Col span={10}>
-                    <Typography.Title level={2} style={{ color: 'blue' }}>
-                        Our Mission
-                    </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, our mission goes beyond transactions; we are dedicated to redefining excellence in the real estate industry. We strive to create lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you're a first-time homebuyer, a seasoned investor, or looking to sell your property, we are here to make your real estate goals a reality.
-                    </Typography.Title>
-                    <Typography.Title level={4}>
-                        At REALBOOK, we understand that finding the perfect home or property is not just about the right features and price; it's about finding a place where dreams come to life and memories are made. As your dedicated real estate partner, we are committed to guiding you through every step of your real estate journey, ensuring a seamless and rewarding experience.
-                    </Typography.Title>
+                <Col span={11} >
+                    <img style={{ width: 500 }} src='https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
                 </Col>
+            </Row>
+            <Row style={{ padding: padding }}>
+                <Col span={11}>
+                    <img style={{ width: 500 }} src='https://images.unsplash.com/photo-1628133287836-40bd5453bed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+                </Col>
+                <Col span={2}></Col>
+                <Col span={11} style={{ padding: padding }}>
+                    <Typography.Title level={2} >
+                        Community Involvement
+                    </Typography.Title>
+                    <Typography>
+                        At REALBOOK, we believe in giving back to the community that has supported us. Learn more about our community initiatives and how we are making a positive impact.
+                    </Typography>
+                </Col>
+            </Row>
+            <Row>
+                <Typography.Title level={2} >
+                    How It Works
+                </Typography.Title>
+
+                <Typography >
+                    Signup and Add Ventures:
+                </Typography>
+                <Typography>
+                    At REALBOOK, we believe in giving back to the community that has supported us. Learn more about our community initiatives and how we are making a positive impact.
+                </Typography>
             </Row>
         </Layout >
     );
