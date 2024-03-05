6d789938c3d5409d4538ddbdaa8830789a75c149
kureti Namah sivaya <kuretinamahsivaya7@gmail.com>
2024-03-05 11:42:13 +0530

    Add prospects

--- src/app/api/prospects/route.ts
+++ src/app/api/prospects/route.ts
@@ -1,5 +1,4 @@
 import prisma from "@/lib/prisma";
-<<<<<<< HEAD
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: Request, res: Response) {
@@ -10,41 +9,6 @@ export async function POST(req: Request, res: Response) {
     return NextResponse.json(prospect);
   }
   export async function GET(req: Request, res: Response) {
-=======
-import { NextResponse } from "next/server";
-
-export async function GET(req: Request, res: Response) {
->>>>>>> main
     const prospects = await prisma.prospect.findMany({});
     return NextResponse.json(prospects);
   }
-
-<<<<<<< HEAD
-
-// export async function POST(req: Request, res: Response) {
-//   const engagementBody = await req.json();
-//   const engagement = await prisma.engagement.create({
-//     data: {
-//       user: {
-//         connect: {
-//           id: engagementBody.userId,
-//         },
-//       },
-//       item: {
-//         connect: {
-//           id: engagementBody.requestId,
-//         },
-//       },
-//     },
-//   });
-//   return NextResponse.json(engagement);
-// }
-=======
-  export async function POST(req: Request, res: Response) {
-    const prospectBody = await req.json();
-    const prospect = await prisma.prospect.create({
-      data: prospectBody,
-    });
-    return NextResponse.json(prospect);
-  }
->>>>>>> main
